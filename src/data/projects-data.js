a3968e4894bef5abf3d196ca421971f5606bde52
BreatheTempest <zabulonov.a@gmail.com>
2022-11-07 20:05:03 +0200

    add home page

---
+++ src/data/projects-data.js
@@ -0,0 +1,32 @@
+export const projects = [
+	{
+		day: 17,
+		name: 'Movie App',
+		slug: 'movie-app',
+	},
+	{
+		day: 18,
+		name: 'Background Slider',
+		slug: 'background-slider',
+	},
+	{
+		day: 19,
+		name: 'Theme Clock',
+		slug: 'theme-clock',
+	},
+	{
+		day: 20,
+		name: 'Button Ripple Effect',
+		slug: 'button-ripple-effect',
+	},
+	{
+		day: 21,
+		name: 'Drag N Drop',
+		slug: 'drag-n-drop',
+	},
+	{
+		day: 22,
+		name: 'Drawing App',
+		slug: 'drawing-app',
+	},
+];
