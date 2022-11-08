6c63ed0a3e3a7db1e6947e7728bc52ad35ce9d23
BreatheTempest <zabulonov.a@gmail.com>
2022-11-08 20:05:08 +0200

    add movie component

--- src/components/movie-app/App.tsx
+++ src/components/movie-app/App.tsx
@@ -4,6 +4,7 @@ import {
   useQuery,
 } from '@tanstack/react-query';
 import axios from 'axios';
+import useHover from '../../utils/useHover';
 
 interface Movie {
   title: string;
@@ -11,12 +12,18 @@ interface Movie {
   vote_average: number;
   overview: string;
   id: number
+  poster_path: string
 }
 
 const queryClient = new QueryClient();
 
+async function fetchMovies(url: string) {
+  const res = await axios.get(url);
+  return res.data;
+}
+
 const url =
-  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ca98a3b8c6446c719f9025aed016ff10';
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ca98a3b8c6446c719f9025aed016ff10&page=1';
 
 export default function App() {
   return (
@@ -27,11 +34,33 @@ export default function App() {
 }
 
 function MovieApp() {
-  const { isLoading, error, data, isFetching } = useQuery(['movies'], () =>
-    axios.get(url).then((res) => res.data)
-  );
+  const { isLoading, error, data, isFetching } = useQuery(['movies'], () => fetchMovies(url));
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error</div>;
 
-  return <div>{data?.results.map((movie: Movie) => (<div key={movie.id}>{movie.title}</div>))}</div>;
+  return <div className='grid grid-cols-fill-40 gap-4 items-center justify-center'>{data?.results.map((movie: Movie) => (<Movie key={movie.id} movie={movie} />))}</div>;
+}
+
+function Movie({ movie }: { movie: Movie }) {
+  const [hoverRef, isHovered] = useHover<HTMLDivElement>();
+
+  return (
+    <div ref={hoverRef} className='bg-sky-800 overflow-hidden relative'>
+      <img className='w-auto object-cover'
+        src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}
+        `}
+        alt={`Movie poster for ${movie.title}`} />
+      <div className={`p-4 absolute w-full bottom-0 bg-sky-800 `}>
+        <div className='flex justify-between'>
+          <h2 className='text-gray-100 '>{movie.title}</h2>
+          <p>{movie.vote_average}</p>
+        </div>
+
+
+
+      </div>
+      <div className={`absolute bg-gray-100 text-gray-900  ${isHovered ? 'right-0 left-0 bottom-0' : 'translate-y-9'} transition max-h-full p-4`}>{movie.overview}</div>
+    </div>
+  )
+
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		gridTemplateColumns: {
+			'fill-40': 'repeat(auto-fit, minmax(300px, 1fr))',
+		},
 	},
 	plugins: [],
-}
+};
